3da9d36f1750fb181e123349f244e2e0666ab81c
kuldeepvarma7413 <kuldeep.12110095@lpu.in>
2022-09-04 12:36:22 +0530

    update

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function c(){
+    var element = document.getElementById('code_inp');
+    element.innerHTML = "Code in C";
+
+
+}
+
+function cplus(){
+    var element = document.getElementById('code_inp');
+    element.innerHTML = "Code in C++";
+
+}
+
+function python(){
+    var element = document.getElementById('code_inp');
+    element.innerHTML = "Code in Python";
+
+}
